caef13e40ab9f9554166d4bc3a700211c1996084
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-11-18 19:03:20 -0600

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,14 +1,26 @@
-class Arrow extends BaseClass {
+class Arrow {
     constructor(x,y){
-      super(x,y,50,50);
-         // this.arrow = loadImage("")
+      //var options = {
+        //'restitution':0.8,
+      //  'friction':1.0,
+        //'density':1.0
+   // }
+    this.body = Bodies.rectangle(x,y,50,50,)
+      this.Arrow = loadImage("images/Arrow.png");
+      this.width = 50;
+      this.height = 50;
     }
   
     display() {
+      
       //this.body.position.x = mouseX;
       //this.body.position.y = mouseY;
-  
-      super.display();
+      var pos = this.body.position
+      push()
+      translate(pos.x,pos.y)
+      imageMode(CENTER)
+     image(this.Arrow,0,0,this.width,this.height)
 
+     pop()
     }
   }
