3e6fefaf866ffb8cceb0a432f4474ca453bf1ef3
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-11-18 19:02:43 -0600

    Update bow.js

--- bow.js
+++ bow.js
@@ -1,4 +1,4 @@
-class bow{
+class Bow{
     constructor(bodyA , pointB){
         var options = {
             bodyA: bodyA,
@@ -6,11 +6,17 @@ class bow{
             stiffness: 0.04,
             length: 10
         }
-       // this.bow = loadImage("");
+               this.bow = loadImage("images/Bow.png");
+               this.pointB = pointB;
        this.bow = Constraint.create(options);
        World.add(world, this.bow);
     }
     display(){
-
+        strokeWeight(7)
+        stroke("black")
+       var pointA  = this.bow.bodyA.position
+        var pointB = pointB;
+       line(pointA.x,pointA.y,pointB.x,pointB.y) 
+        
     }
 }
